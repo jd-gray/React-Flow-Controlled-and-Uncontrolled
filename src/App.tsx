07a0ba23282287bbf305c1770ca61601eaec0059
Jared Gray <jared@jaredgray.us>
2024-04-23 16:08:33 -0700

    Build out controlled flow with basic ReactFlow setup
    This flow uses state management for nodes and edges and adds a random node to the canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,33 @@
 import './App.css'
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import 'reactflow/dist/style.css';
+import ControlledFlow from "./ControlledFlow.tsx";
+
+export const initialNodes = [
+  {
+    id: '1',
+    type: 'input',
+    data: { label: 'Input Node' },
+    position: { x: 250, y: 25 },
+  },
+
+  {
+    id: '2',
+    data: { label: <div>Default Node</div> },
+    position: { x: 100, y: 125 },
+  },
+  {
+    id: '3',
+    type: 'output',
+    data: { label: 'Output Node' },
+    position: { x: 250, y: 250 },
+  },
+];
+
+export const initialEdges = [
+  { id: 'e1-2', source: '1', target: '2' },
+  { id: 'e2-3', source: '2', target: '3', animated: true },
+];
 
 const App = () => {
   return (
@@ -10,7 +37,7 @@ const App = () => {
         <Link to="/uncontrolled-flow">Uncontrolled Flow</Link>
       </nav>
       <Routes>
-        <Route path="/controlled-flow" element={<div>Controlled flow</div>} />
+        <Route path="/controlled-flow" element={<ControlledFlow />} />
         <Route path="/uncontrolled-flow" element={<div> Uncontrolled flow</div>} />
       </Routes>
     </BrowserRouter>
---
+++ src/ControlledFlow.tsx
@@ -0,0 +1,38 @@
+import { Background, Controls, MiniMap, Panel, ReactFlow, Node, Edge } from "reactflow";
+import { useState } from "react";
+import { initialEdges, initialNodes } from "./App.tsx";
+
+const ControlledFlow = () => {
+  const [nodes, setNodes] = useState<Node[]>(initialNodes);
+  const [edges, _] = useState<Edge[]>(initialEdges);
+
+  // Function to add a new node
+  const onAddNode = () => {
+    const newNode = {
+      id: (nodes.length + 1).toString(), // Simple ID generation
+      data: { label: `Node ${nodes.length + 1}` },
+      position: {
+        x: Math.random() * 500,
+        y: Math.random() * 500,
+      },
+    };
+    setNodes((nds) => nds.concat(newNode));
+  };
+
+  return (
+    <div style={{ height: 800 }}>
+      <ReactFlow nodes={nodes} edges={edges} fitView>
+        <MiniMap />
+        <Controls />
+        <Background />
+        <Panel position='top-left'>
+          <button onClick={onAddNode}>
+            Add Node
+          </button>
+        </Panel>
+      </ReactFlow>
+    </div>
+  );
+}
+
+export default ControlledFlow;
