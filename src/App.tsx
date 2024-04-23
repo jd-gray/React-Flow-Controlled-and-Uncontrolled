782ea42deca869ce49ae7f6ee0eca297b3c7dd7c
Jared Gray <jared@jaredgray.us>
2024-04-23 15:47:46 -0700

    Setup BrowserRouter to navigate between controlled and uncontrolled react flow views

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import 'reactflow/dist/style.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <nav>
+        <Link to="/controlled-flow">Controlled Flow</Link> |
+        <Link to="/uncontrolled-flow">Uncontrolled Flow</Link>
+      </nav>
+      <Routes>
+        <Route path="/controlled-flow" element={<div>Controlled flow</div>} />
+        <Route path="/uncontrolled-flow" element={<div> Uncontrolled flow</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
